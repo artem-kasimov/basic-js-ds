461f0b3678ce2966a1b8333cf65fd199612bea58
Artem Kasimov <xartem.kx@gmail.com>
2022-10-17 17:37:45 +0500

    feat: queue in process

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -14,23 +14,54 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
+  constructor() {
+    this._queue = {}
+  }
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._queue
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    if (Object.keys(this._queue).length === 0) {
+      this._queue.value = value
+      this._queue.next = null
+    } else {
+      if (this._queue.next !== null) {
+        let temp = this._queue.next
+        while (temp === null) {
+          temp = temp.next
+        }
+        temp.next = {
+          value,
+          next: null,
+        }
+      } else {
+        this._queue.next = {
+          value,
+          next: null,
+        }
+      }
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let output = this._queue.value
+
+    this._queue = this._queue.next
+
+    return output
   }
 }
 
+// const queue = new Queue()
+// queue.enqueue(1)
+// console.log(queue.getUnderlyingList())
+// queue.enqueue(3)
+// console.log(queue.getUnderlyingList())
+// queue.enqueue(5)
+// console.log(queue.getUnderlyingList())
+// queue.dequeue()
+// console.log(queue.getUnderlyingList())
 module.exports = {
-  Queue
-};
+  Queue,
+}
